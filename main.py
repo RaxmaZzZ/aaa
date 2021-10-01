4660856462aa4744c43663a2dbc68639d39033c9
RaxmaZzZ <one-of-world-1@bk.ru>
2021-10-01 10:51:25 +1000

    1

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 a = 1
+print(type(a))
+b = .5
+print(type(b))
+
+c, g = 'привет', "Мир"
+print(type(c))
+
+v = a + b
+print(v)
+
+n = c + g # сложение
+print(n)
+
+print(10 + 20)
+print(10 - 20)
+print(10 / 20)
+print(10 // 20)
+print(10 * 20)
+print(10 ** 2)
+print(10 % 2)
+print(10 ** 2)
+print(10 % 2)
+print(math.sqrt(10))
+
+h = True
+print(type(h))
+
+lst = [1, 3, 'd', 123, 'Привет']
+print(type(lst))
+
+print(lst[4])
